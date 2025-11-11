f6c372b58c8937c8e4f1b9bcda04079052bd52e0
Oleksandr Chumak <oleksandr.chumak@sikt.no>
2025-11-11 18:44:20 +0100

    Add triggers

--- sql/startup/02_create_schema.sql
+++ sql/startup/02_create_schema.sql
@@ -2,9 +2,18 @@ ALTER SESSION SET CONTAINER = XEPDB1;
 
 CONNECT airline/strongpassword@XEPDB1;
 
+CREATE SEQUENCE audit_seq start with 1 INCREMENT BY 1;
 CREATE SEQUENCE flight_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE passenger_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE ticket_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE audits
+(
+    command_id  NUMBER PRIMARY KEY,
+    table_name  VARCHAR2(50) NOT NULL,
+    action_date VARCHAR2(50) NOT NULL,
+    command     VARCHAR2(10) NOT NULL
+);
 
 CREATE TABLE flights
 (
@@ -76,6 +85,35 @@ CREATE OR REPLACE TRIGGER ticket_trigger
 BEGIN
     IF :NEW.ticket_id IS NULL THEN
         SELECT ticket_seq.NEXTVAL INTO :NEW.ticket_id FROM DUAL;
+    END IF;
+END;
+/
+
+CREATE OR REPLACE TRIGGER audit_tickets_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON tickets
+    FOR EACH ROW
+DECLARE
+    v_command VARCHAR2(10);
+BEGIN
+    IF INSERTING THEN
+        v_command := 'Insert';
+    ELSIF UPDATING THEN
+        v_command := 'Update';
+    ELSIF DELETING THEN
+        v_command := 'Delete';
+    END IF;
+
+    INSERT INTO audits (command_id,table_name, action_date, command)
+    VALUES (audit_seq.NEXTVAL, 'TICKETS', TO_CHAR(SYSDATE), v_command);
+END;
+/
+
+CREATE OR REPLACE TRIGGER check_ticket_price_trigger
+    BEFORE INSERT OR UPDATE OF price ON tickets
+    FOR EACH ROW
+BEGIN
+    IF :NEW.price < 10 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Error: Ticket price cannot be less than 10. Entered price: ' || :NEW.price);
     END IF;
 END;
 /
